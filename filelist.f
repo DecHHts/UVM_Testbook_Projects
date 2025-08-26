692e7e9f4057420a7d74d308369c9b0a27e67cbf
HE YUXUAN <christopherhyx@outlook.com>
2025-08-26 09:30:31 +0800

    1_Aug26th_2025

--- filelist.f
+++ filelist.f
@@ -1,4 +1,4 @@
 +incdir+$UVM_HOME/src
 $UVM_HOME/src/uvm_pkg.sv
-/home/ICer/heyuxuan_prjs/simple_prjs/dut.sv
-/home/ICer/heyuxuan_prjs/simple_prjs/top_tb.sv
+/home/ICer/heyuxuan_prjs/UVM_Testbook_Projects/dut.sv
+/home/ICer/heyuxuan_prjs/UVM_Testbook_Projects/top_tb.sv
